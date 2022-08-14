9edaeaa2c21a5e313d1625838d0ba1347913c4ca
Leone Oliveira <falecom@leone.tec.br>
2022-08-14 08:43:32 -0300

    Faz o botão calcular funcionar corretamente
    Implementando as primeiras lógicas de cálculo para quando o valor no
    cartão/parcelado for o mesmo ou menor que à vista, demais lógicas devemm ser inplementadas posteriormente.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 const installmentsDiv = $('#installments-div'), installments = $('#installments'), value = $('#installments-value')
 const quantity = $('#installments-quantity'), valueInCredit = $('#value-in-credit'), valueInCash = $('#value-in-cash')
-const inCardDiv = $('#in-card-div'), inCard = $('#in-card')
+const inCardDiv = $('#in-card-div'), inCard = $('#in-card'), textInCard = $('#text-in-card')
 
-$('.needs-validation').on('submit', function (e) {
+$('#calculator').on('submit', function (e) {
     e.preventDefault()
     if (!this.checkValidity()) {
         e.stopPropagation()
+    }else{
+        let credit = parseInt(valueInCredit.val()), cash = parseInt(valueInCash.val())
+        let result = $('#result'), text = 'É mais vantajoso pagar '
+        if (credit <= cash) {
+            text += textInCard.html() + '!'
+            result.html(text).addClass('alert-success')
+        }
+        result.removeClass('d-none')
     }
 
     $(this).addClass('was-validated')
@@ -23,6 +31,7 @@ installments.on('click', () =>  {
         quantity.attr('required', '').on('change', changeInstallments)
         valueInCredit.attr('readonly', '')
         inCardDiv.removeClass('d-none')
+        textInCard.html('parcelado')
     } else {
         installmentsDiv.addClass('d-none').removeClass('d-flex')
         value.removeAttr('required')
@@ -30,5 +39,6 @@ installments.on('click', () =>  {
         valueInCredit.removeAttr('readonly')
         inCardDiv.addClass('d-none')
         inCard.prop('checked', false)
+        textInCard.html('no cartão')
     }
 })
